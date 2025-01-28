4cb734059220cba05422bd1e289be224b85f0f0b
Nektarios1667 <nektarioslavdas@gmail.com>
2025-01-27 20:08:39 -0500

    Log files older than 2 days are automatically deleted

--- Pixxl/Logger.cs
+++ Pixxl/Logger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Pixxl
 {
@@ -11,12 +12,24 @@ namespace Pixxl
         private static string filename = $"log-{timestamp}.txt";
         static Logger()
         {
+            // Loading
             if (!Directory.Exists("Logs"))
             {
                 Directory.CreateDirectory("Logs");
             }
             File.WriteAllText($"Logs/{filename}", "");
             Log("Initialized Logger");
+
+            // Clearing old logs
+            foreach (string file in Directory.GetFiles("Logs", "log-*.txt"))
+            {
+                string[] parts = Path.GetFileNameWithoutExtension(file).Split('-');
+
+                if (parts.Length > 2 && int.TryParse(parts[1], out int day) && Math.Abs(day - DateTime.Now.Day) > 2)
+                {
+                    File.Delete(file);
+                }
+            }
         }
         public static void Log(params string[] texts)
         {
